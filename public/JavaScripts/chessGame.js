f511cba98c2b16f2eb37537e9c93c916a9004a28
Rohit <ojha954545@gmail.com>
2025-08-14 03:09:52 -0700

    Integrating chess.js and initializing game variables

--- public/JavaScripts/chessGame.js
+++ public/JavaScripts/chessGame.js
@@ -1,21 +1,8 @@
+const { Chess } = require("chess.js");
 const socket = io();
+const chess = new Chess();
+const boardElement = document.querySelector("#chessboard");
 
-socket.emit("Invitation");
-socket.on("Invitation Received", () => {
-    console.log("Invitation Received, Accepted!")
-});
-
-
-// Connection event handlers
-// socket.on('connect', () => {
-//     console.log('Connected to server');
-//     socket.emit("Invitation");
-// });
-
-// socket.on('connect_error', (error) => {
-//     console.error('Connection error:', error);
-// });
-
-// socket.on('disconnect', () => {
-//     console.log('Disconnected from server');
-// });
+let draggedPiece = null;
+let sourceSquare = null;
+let playerRole = null;
